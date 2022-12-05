1bc01c4cff59a32849f2e930521fef3635656cfc
bhargavasai227 <112853876+bhargavasai227@users.noreply.github.com>
2022-12-05 21:26:36 +0530

    Add files via upload

--- result.js
+++ result.js
@@ -1,17 +1,24 @@
 const express = require("express");
 const body = require("body-parser");
 const mongoose = require("mongoose");
-const { stringify } = require("querystring");
+const expressLayouts = require('express-ejs-layouts');
 const app =express();
 app.use(body.urlencoded({ extended: false }));
 app.use(express.static("public"));
+app.use(expressLayouts);
+app.set('veiw engine', 'ejs')
+
 
 app.get("/",function(req,res){
-res.sendFile(__dirname+"/index.html")
+res.sendFile(__dirname+"/public/index.html")
+});
+app.get("/search",(req,res)=>{
+res.sendFile(__dirname+"/public/search.html")
 });
 
-app.listen(process.env.PORT);
-
+app.listen(3000||process.env.PORT,()=>{
+  console.log("server started");
+});
 
 const uri = "mongodb+srv://bhargav:bhargav%40227@cluster0.r0cqany.mongodb.net/numbers";
 mongoose.connect(uri, { useNewUrlParser: true });
@@ -24,15 +31,43 @@ const rollscema = new mongoose.Schema({
 // db model or table or collection
 const rollNumber=mongoose.model("rollNumber",rollscema);
 
-// inseting to db
+// inseting to db and redirecting to picture
 app.post("/",function(req,res){
 var rNo =req.body.input;
+console.log(rNo);
 const roll = new rollNumber({
   number:rNo
   });
-  roll.save();
-  res.redirect("http://ecap.pace.ac.in//StudentPhotos/"+rNo+".jpg");
+  roll.save()
+  // res.redirect("http://ecap.pace.ac.in//StudentPhotos/"+rNo+".jpg");
+  res.end();
 
 });
-
- 
+//search query
+app.post("/search",(req,res)=>{
+  var item=req.body.input1;
+  console.log(item);
+  rollNumber.findOne({number:item},function(err,data){
+    if(err){
+      console.log(err);
+      
+    }
+    else{
+      if(data==null){
+        
+      console.log("not found");
+      // res.write("not found in data base");
+      res.sendFile(__dirname+"/public/failure.html")
+        
+         
+      }
+     else{
+       console.log("found");
+      //  res.write("found in database");
+       res.sendFile(__dirname+"/public/success.html")
+      
+     }
+    }
+  })
+  
+});
